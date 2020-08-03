77296c8edcd2ce83f73f257e7b5e9a73a78ed0c6
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:44:52 -0400

    imports

---
+++ src/components/Compare.js
@@ -0,0 +1,4 @@
+import React, { useState, useContext } from "react";
+import WordSearchBar from "./WordSearchBar";
+import WordContext from "./contexts/WordContext";
+import Nav from "react-bootstrap/Nav";
