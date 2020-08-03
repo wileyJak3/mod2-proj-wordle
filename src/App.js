dd98f4933a60989af390f51f2c1242daccbd582c
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:05:07 -0400

    imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import ApiCall from "./components/ApiCall";
+import WordSearchBar from "./components/WordSearchBar";
