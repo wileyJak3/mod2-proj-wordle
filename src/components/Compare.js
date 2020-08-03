003686244d56f327c4412277758bd27da9cd1639
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:45:46 -0400

    imports

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -4,4 +4,6 @@ import WordContext from "./contexts/WordContext";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
