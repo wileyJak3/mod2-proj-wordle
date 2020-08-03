20c6e72a243d79e8638417a1c6adc866e05f7ccb
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:09:38 -0400

    imports

--- src/components/UrbanMainCard.js
+++ src/components/UrbanMainCard.js
@@ -2,3 +2,6 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
+import { Container, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import ApiContext from "./contexts/ApiContext";
