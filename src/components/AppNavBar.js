2fb82ac935aa31a04f770050550013eac9947573
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:43:57 -0400

    AppNavBar

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import WordSearchBar from "./WordSearchBar";
 import WordContext from "./contexts/WordContext";
-
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
@@ -9,12 +8,15 @@ import Container from "react-bootstrap/Container";
 import ApiCall from "./ApiCall";
 import ApiContext from "./contexts/ApiContext";
 import DictionaryCard from "./DictionaryCard";
-mport MainDictionaryCard from "./MainDictionaryCard";
+import MainDictionaryCard from "./MainDictionaryCard";
 import UrbanMainCard from "./UrbanMainCard";
-import { BrowserRouter as Router, Switch, Route, Link } from 
-react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Compare from './Compare'
 import './styles/nav-bar.css'
+
+
+
+
 
 function AppNavbar() {
   //
@@ -23,7 +25,6 @@ function AppNavbar() {
   const wordData = useContext(WordContext);
   // const [googleData,urbanData,quoteData] = allApiData
   console.log(allApiData[0].googleData);
-
   console.log(allApiData[1].urbanData);
   console.log(allApiData[2].quoteData);
 
@@ -35,43 +36,40 @@ function AppNavbar() {
   return (
     <div>
       <Router>
-        <Na
-
-div>
-<Navbar.Brand><Link to="/">Wordle</Link></Navbar.Brand>
-</div>
-
-{/* <div className="nav-bar-flex"> */}
-<Nav>
-  <Nav.Link>
-    <Link to="/Urban">Urban</Link>
-  </Nav.Link>
-</Nav>
-<Nav>
+        <Navbar bg="dark" variant="dark">
+        <div>
+            <Navbar.Brand><Link to="/">Wordle</Link></Navbar.Brand>
+          </div>
+         
+          {/* <div className="nav-bar-flex"> */}
+            <Nav>
+              <Nav.Link>
+                <Link to="/Urban">Urban</Link>
+              </Nav.Link>
+            </Nav>
+            <Nav>
               <Nav.Link>
                 <Link to="/Dictionary">Dictionary</Link>
               </Nav.Link>
             </Nav>
-            <Nav></Nav>
-
-            {/* <WordContext.Provider value={{ wordContext, setWordContext }}> */}
-            <WordSearchBar />
+            <Nav>
+              {/* <WordContext.Provider value={{ wordContext, setWordContext }}> */}
+              <WordSearchBar />
               {/* {wordContext ? <ApiCall word={wordContext} /> : "Loading..."} */}
-
               {/* </WordContext.Provider> */}
             </Nav>
            
 
-           {/*//! Incomplete Component */}
-           {/* <Nav>
-             <Nav.Link>
-               <Link to="/Compare">Compare</Link>
-             </Nav.Link>
-           </Nav> */}
-           
-         {/* </div> */}
-
-         {/* {allApiData[1].urbanData ? <UrbanMainCard />:null } */}
+            {/*//! Incomplete Component */}
+            {/* <Nav>
+              <Nav.Link>
+                <Link to="/Compare">Compare</Link>
+              </Nav.Link>
+            </Nav> */}
+            
+          {/* </div> */}
+        </Navbar>
+        {/* {allApiData[1].urbanData ? <UrbanMainCard />:null } */}
         <Switch>
           <Route path="/Dictionary">
             {allApiData[0].googleData ? <MainDictionaryCard /> : null}
@@ -89,8 +87,7 @@ div>
           { wordData.wordContext ? <h1 id ="wordSearch">{wordData.wordContext}</h1>:null}
             {allApiData[2].quoteData ? <h1 id ="quote">{`Quote: ${allApiData[2].quoteData.content}`}</h1>:<h1 id = "intro" >Wordle</h1>}
           </Route>
-
-           {/* <DictionaryCard/> */}
+          {/* <DictionaryCard/> */}
         </Switch>
       </Router>
     </div>
@@ -98,8 +95,7 @@ div>
     //   <WordContext.Provider value={{ wordContext, setWordContext }}>
     //       <WordSearchBar/>
     //  </WordContext.Provider>
-
-     // </div>
+    // </div>
   );
 }
 
