8e000243f8a5ff10546ca8ea6fc79b2ef756f010
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:42:50 -0400

    search bar

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -89,3 +89,12 @@ div>
           { wordData.wordContext ? <h1 id ="wordSearch">{wordData.wordContext}</h1>:null}
             {allApiData[2].quoteData ? <h1 id ="quote">{`Quote: ${allApiData[2].quoteData.content}`}</h1>:<h1 id = "intro" >Wordle</h1>}
           </Route>
+
+           {/* <DictionaryCard/> */}
+        </Switch>
+      </Router>
+    </div>
+    // <div className="nav-bar-flex">
+    //   <WordContext.Provider value={{ wordContext, setWordContext }}>
+    //       <WordSearchBar/>
+    //  </WordContext.Provider>
