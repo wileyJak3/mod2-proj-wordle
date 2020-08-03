c34c6883fda4db53b7a451b2fcff67a3f0ff3082
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:19:15 -0400

    urbanMainCard

--- src/components/UrbanMainCard.js
+++ src/components/UrbanMainCard.js
@@ -19,16 +19,14 @@ function MainDictionCard() {
     return urbanData.map((Element) => (
       <div id="main-card-urban">
         {console.log("this is the element", { Element })}
-
         <li>
           <p>{Element.definition}</p>
           <p id="example">{Element.example}</p>
           <p id="author">
             by {Element.author} {Element.written_on.substring(0, 10)}
           </p>
-          <div id="like-dislike"></div>
-
-          Row>
+          <div id="like-dislike">
+            <Row>
               <Col>
                 <p>Likes:{Element.thumbs_up}</p>
               </Col>
@@ -36,59 +34,56 @@ function MainDictionCard() {
                 <p id="left-border">Dislikes:{Element.thumbs_down}</p>
               </Col>
             </Row>
-
-            </div>
+          </div>
         </li>
       </div>
     ));
 
     //   for(let i = 0; i<=2;i++){
     //    return <div className="main-card-inner">
+    //     <h6 id ="part-of-speach">{googleData.meanings[i].partOfSpeech}</h6>
+    //     <p>{googleData.meanings[i].definitions[0].definition}</p>
+    //     <p id ="example">{googleData.meanings[i].definitions[0].example}</p>
+    //     </div>
 
-    return (
-      <div>
-        <Container>
-          <Row>
-            <Col>
+    //   }
+  }
 
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
             <Card>
               <Card.Header>
                 <Nav variant="tabs" defaultActiveKey="#first">
                   <Nav.Item>
-                    
-
-                  <Nav.Link href="#first">Definition</Nav.Link>
+                    <Nav.Link href="#first">Definition</Nav.Link>
                   </Nav.Item>
                   <Nav.Item>
                     <Nav.Link href="#link">Link</Nav.Link>
-
-                    </Nav.Item>
+                  </Nav.Item>
                   <Nav.Item>
                     <Nav.Link href="#disabled" disabled>
                       Disabled
                     </Nav.Link>
-
-                    /Nav.Item>
+                  </Nav.Item>
                 </Nav>
               </Card.Header>
               <Card.Body>
-                <div id="line-border"></div>
-
-                <h1 id="title-header-urban">{urbanData[0].word}</h1>
+                <div id="line-border">
+                  <h1 id="title-header-urban">{urbanData[0].word}</h1>
                   <ol>{printMeaning()}</ol>
                 </div>
                 {/* {urbanData ? printMeaning() : null} */}
                 <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
+              </Card.Body>
             </Card>
           </Col>
-          </Card.Body>
-            </Card>
-          </Col>
-           </Row>
-           </Container>
-         </div>
-       );
-     }
-     
-     export default MainDictionCard;
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default MainDictionCard;
