f74c20056600a0a49a27007904f556775e58069a
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:09:53 -0400

    imports

--- src/components/UrbanMainCard.js
+++ src/components/UrbanMainCard.js
@@ -5,3 +5,7 @@ import Button from "react-bootstrap/Button";
 import { Container, Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import ApiContext from "./contexts/ApiContext";
+import { useContext } from "react";
+import "./styles/main-card-urban.css";
+
+function MainDictionCard() {
