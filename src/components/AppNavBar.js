9364dc0e7ae8d80e183a523d20c738b1341d34e0
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 22:29:04 -0400

    imports

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -1,3 +1,7 @@
 import React, { useState, useContext } from "react";
 import WordSearchBar from "./WordSearchBar";
 import WordContext from "./contexts/WordContext";
+
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
