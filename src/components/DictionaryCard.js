c862ca82fe4d74ae9709448207527dc470dce048
wileyJak3 <wileyJak3@gmail.com>
2020-08-02 23:02:39 -0400

    DictionaryCard.js

---
+++ src/components/DictionaryCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
+import { Col} from "react-bootstrap";
+
+function DictionaryCard() {
+  return (
+    <div>
+
+     <Col>
+      <Container>
+        <Card>
+          <Card.Header>
+            <Nav variant="tabs" defaultActiveKey="#first">
+              <Nav.Item>
+                <Nav.Link href="#first">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#disabled" disabled>
+                  Disabled
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>Special title treatment</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+      </Container>
+      </Col>
+      
+    </div>
+  );
+}
+
+export default DictionaryCard;
